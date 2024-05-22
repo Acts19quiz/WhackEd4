ceaf33b1c8dca65f8d70fc1d1cd9faa96f51fbeb
Acts 19 quiz <N/A>
2024-05-22 15:16:41 -0500

    Add table adjustments for Doom Legacy and ZDaemon Heretic actors; remove forth digit subversion (i.e., v1.2.3).

--- build-release-package.cmd
+++ build-release-package.cmd
@@ -4,7 +4,7 @@ set app_description=A Windows Dehacked editor.
 set app_name=WhackEd4
 set app_name_lower=whacked4
 
-set app_version=1.2.2.5
+set app_version=1.2.3
 set app_version_value=1.2.2
 set app_version_title=1.2.2
 
